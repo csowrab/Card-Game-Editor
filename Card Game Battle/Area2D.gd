571a4cbe13fb6bfc5278a9c6b3d331ac0b96b9d6
Sowrab Chowdhury <csowrab@gmail.com>
2021-09-09 18:58:17 +0100

    Added drag effect of main cards
    Cards can now be dragged by right-clicking on them.

--- Card Game Battle/Area2D.gd
+++ Card Game Battle/Area2D.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var a = 1
+var b = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,13 +13,18 @@ func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	pass
+	dragCard()
 	
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
-	and event.button_index == BUTTON_LEFT \
+	and event.button_index == BUTTON_RIGHT \
 	and event.is_pressed():
 		toggleTexture()
+		
+	elif event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		toggleDrag()
 		
 
 func toggleTexture():
@@ -28,7 +34,16 @@ func toggleTexture():
 	else:
 		bullet_sprite.set_texture(preload("res://Textures/CardCover.png"))
 		a = 1
-		
+
+func toggleDrag():
+	b = !b
+	
+func dragCard():
+	if (b == true):
+		var mouse_x = get_viewport().get_mouse_position().x
+		var mouse_y = get_viewport().get_mouse_position().y
+		position = Vector2(mouse_x,mouse_y)
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
