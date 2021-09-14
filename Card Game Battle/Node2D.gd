465bfafecb89e427c578d3effba583394a0b8d5d
Sowrab Chowdhury <csowrab@gmail.com>
2021-09-14 20:23:49 +0100

    Added a timer, and attepting to add the "arrow".
    Biggest problem is key detection. We want key_press to be detected only once.

--- Card Game Battle/Node2D.gd
+++ Card Game Battle/Node2D.gd
@@ -5,19 +5,15 @@ onready var Text2 = get_node("Display2").get_node("Label")
 onready var Text3 = get_node("Display3").get_node("Label")
 onready var Text4 = get_node("Display4").get_node("Label")
 
+var turn = 1;
+
 var _timer = null
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Text1.text = '0';
 	Text2.text = "0";
 	Text3.text = "10";
-	Text4.text = "10";
+	Text4.text = "<-";
 	
 	_timer = Timer.new()
 	add_child(_timer)
@@ -32,21 +28,27 @@ func _ready():
 
 func _physics_process(delta):	
 	Text3.text = str(int(_timer.get_time_left()))
-	Text4.text = str(int(_timer.get_time_left()))
+
+func toggleTurn():
+	turn += 1;
+	if (turn % 2 == 0):
+		Text4.text = "->"
+	else:
+		Text4.text = "<-"
 	
 	
 func _input(ev):
 	
-	if ev is InputEventKey and ev.scancode == KEY_K:
+	var changed = false;
+	
+	if  ev is InputEventKey and ev.scancode == KEY_K and (changed == false):
 		Text1.text = str(int(Text1.text) + 1)
+		changed = true;
 		 
 	if ev is InputEventKey and ev.scancode == KEY_L:
 		Text2.text = str(int(Text2.text) + 1)
+		
+	if ev is InputEventKey and ev.scancode == KEY_J:
+		toggleTurn();
 
 
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
